ee9bb53e007f622e96e7282b890a63ff88832b5e
Widyo Tri Cahya Utomo <74693211+tcuber11@users.noreply.github.com>
2024-05-21 09:56:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+import streamlit
+import snowflake.connector
+import pandas
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
